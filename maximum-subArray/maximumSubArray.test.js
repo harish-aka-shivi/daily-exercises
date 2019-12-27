b75dc2db80b5128cc5f286112552d46f38315b72
Harish Rana <r.shivi@yahoo.com>
2019-12-27 13:34:52 +0530

    feat: Solve maximum Subarray problem

---
+++ maximum-subArray/maximumSubArray.test.js
@@ -0,0 +1,9 @@
+import maxSubArray from './maximumSubArray';
+
+describe('Find the largest common prefix', () => {
+  it('should give right result for following cases', () => {
+    expect(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
+    expect(maxSubArray([2, 3])).toBe(5);
+    expect(maxSubArray([-1, 0])).toBe(0);
+  });
+});
