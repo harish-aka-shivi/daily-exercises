796f51356fbf1522435c3ea52058a2c0a746b15f
Harish Rana <r.shivi@yahoo.com>
2019-12-28 17:46:50 +0530

    fix: Make a better solution for bestTimeToSolve

--- time-to-sell-stocks/timeToSellStocks.js
+++ time-to-sell-stocks/timeToSellStocks.js
@@ -3,14 +3,29 @@
  * @param {number[]} prices
  * @return {number}
  */
+// const maxProfit = function (prices) {
+//   let maxSoFar = -Infinity;
+//   for (let i = 0; i < prices.length - 1; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       maxSoFar = prices[j] - prices[i] > maxSoFar ? prices[j] - prices[i] : maxSoFar;
+//     }
+//   }
+//   return maxSoFar > 0 ? maxSoFar : 0;
+// };
+
+// better solution
 const maxProfit = function (prices) {
-  let maxSoFar = -Infinity;
-  for (let i = 0; i < prices.length - 1; i++) {
-    for (let j = i + 1; j < prices.length; j++) {
-      maxSoFar = prices[j] - prices[i] > maxSoFar ? prices[j] - prices[i] : maxSoFar;
+  let maxProfitAmount = 0;
+  let minPrice = +Infinity;
+  prices.forEach((item) => {
+    if (item < minPrice) {
+      minPrice = item;
     }
-  }
-  return maxSoFar > 0 ? maxSoFar : 0;
+    if (item - minPrice > maxProfitAmount) {
+      maxProfitAmount = item - minPrice;
+    }
+  });
+  return maxProfitAmount;
 };
 
 export default maxProfit;
--- time-to-sell-stocks/timetoSellStocks.test.js
+++ time-to-sell-stocks/timetoSellStocks.test.js
@@ -6,6 +6,6 @@ describe('Find the max sum of contiguous array', () => {
     expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5);
     expect(maxProfit([7, 6, 4, 3, 1])).toEqual(0);
     expect(maxProfit([])).toEqual(0);
-    expect(maxProfit(1)).toEqual(0);
+    expect(maxProfit([1])).toEqual(0);
   });
 });
