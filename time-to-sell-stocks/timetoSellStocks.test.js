067a25a157be4e7edb4cd285fc609de19549aa28
Harish Rana <r.shivi@yahoo.com>
2019-12-28 19:00:17 +0530

    feat: Solve timeToSellStock2

--- time-to-sell-stocks/timetoSellStocks.test.js
+++ time-to-sell-stocks/timetoSellStocks.test.js
@@ -1,6 +1,6 @@
 import maxProfit from './timeToSellStocks';
 
-describe('Find the max sum of contiguous array', () => {
+describe('Find the max profit after selling stock once', () => {
   it('should give right result for following cases', () => {
     expect(maxProfit([7, 6, 1, 5, 3, 4])).toEqual(4);
     expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5);
