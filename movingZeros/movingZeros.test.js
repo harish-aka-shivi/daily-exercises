040f89e69a0d35f8b5732f90c36dc7b632429779
Harish Rana <r.shivi@yahoo.com>
2019-12-31 10:01:15 +0530

    style: Fix the eslint error

--- movingZeros/movingZeros.test.js
+++ movingZeros/movingZeros.test.js
@@ -2,7 +2,7 @@ import moveZeroes from './movingZeros';
 
 describe('Should move the zeroes to the end in-place', () => {
   it('should solve the following test cases', () => {
-    let a = [0, 1, 0, 3, 12];
+    const a = [0, 1, 0, 3, 12];
     moveZeroes(a);
     expect(a).toEqual([1, 3, 12, 0, 0]);
   });
