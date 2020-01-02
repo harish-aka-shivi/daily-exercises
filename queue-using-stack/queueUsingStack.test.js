7d541911d05dd5bd5fd6ce6d779df07e34037fdd
Harish Rana <r.shivi@yahoo.com>
2020-01-02 12:29:46 +0530

    feat: implement queue using stack

---
+++ queue-using-stack/queueUsingStack.test.js
@@ -0,0 +1,19 @@
+import MyQueue from './queueUsingStack';
+
+describe('Shoud implement stack using queue', () => {
+  it('pass the following test cases', () => {
+    const queue = new MyQueue();
+    queue.push(0);
+    queue.push(1);
+    queue.push(2);
+    const peek = queue.peek();
+    const pop = queue.pop();
+    const pop2 = queue.pop();
+    const peek2 = queue.peek();
+
+    expect(peek).toEqual(0);
+    expect(pop).toEqual(0);
+    expect(pop2).toEqual(1);
+    expect(peek2).toEqual(2);
+  });
+});
