f0817940ad7c9855870392e1f2960011b7b21a22
Harish Rana <r.shivi@yahoo.com>
2020-01-10 18:32:20 +0530

    refactor: move some programs in appropriate directory

--- array-partition/arrayPartition.test.js
+++
@@ -1,8 +0,0 @@
-import arrayPartition from './arrayPartition';
-
-describe('Should solve the array partition problem', () => {
-  it('should solve the following test cases', () => {
-    expect(arrayPartition([0, 1, 2, 3])).toEqual(2);
-    expect(arrayPartition([1, 4, 3, 2])).toEqual(4);
-  });
-})
