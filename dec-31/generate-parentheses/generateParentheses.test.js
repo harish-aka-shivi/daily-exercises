42ce6829648f588318a1f8e70d7d1c9e48083550
Harish Rana <r.shivi@yahoo.com>
2020-01-10 17:38:34 +0530

    Revert "feat: Solve problems from dec 31 - jan 6 (#15)"
    This reverts commit c075c408c49d27b268413613921a7e986e002c92.

--- dec-31/generate-parentheses/generateParentheses.test.js
+++
@@ -1,9 +0,0 @@
-import generateParentheses from './generateParentheses';
-
-describe('should generate the valid parentheses', () => {
-  it('should pass the following test cases', () => {
-    expect(generateParentheses(3)).toEqual([
-      '()()()', '()(())', '((()))', '(()())', '(())()',
-    ]);
-  });
-});
