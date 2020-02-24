c281276a30ab50a9d133af38165ed17641535938
Harish Rana <r.shivi@yahoo.com>
2020-02-24 16:53:08 +0530

    style: remove unwanted logs

--- feb-24/array-nesting/arrayNesting.js
+++ feb-24/array-nesting/arrayNesting.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 
-// const countCache = {};
-
 const findNestCount = (nums, startIndex) => {
   let count = 0;
   const hashMap = {};
   let reducedAcc = startIndex;
 
   while (!hashMap[reducedAcc]) {
-    // console.log(countCache, startIndex)
     count += 1;
     hashMap[reducedAcc] = true;
     reducedAcc = nums[reducedAcc];
@@ -19,7 +16,7 @@ const findNestCount = (nums, startIndex) => {
   return count;
 };
 
-const arrayNesting = function (nums) {
+const arrayNesting = (nums) => {
   let longestCount = Number.MIN_SAFE_INTEGER;
 
   nums.forEach((item) => {
